2277dc0b2439fb2bd056314860e5c80de3745f0d
Devnilson <dehjr1990@gmail.com>
2024-06-03 15:48:08 -0300

    [atualizando portifolio]

--- src/componentes/tecnologias/Item.jsx
+++ src/componentes/tecnologias/Item.jsx
@@ -1,20 +1,11 @@
-import React, { useState, useEffect } from 'react';
 import { Tecnologia } from './styles.js';
 
 function Item(props) {
-    const [imageSrc, setImageSrc] = useState('');
-
-    useEffect(() => {
-        import(`../assets/img/${props.image}.png`)
-            .then(image => setImageSrc(image.default))
-            .catch(err => console.error('Erro ao carregar a imagem:', err));
-    }, [props.image]);
-
     return (
         <Tecnologia>
-            {imageSrc ? <img src={imageSrc} alt={props.alt} /> : 'Carregando...'}
+            <img src={`src/assets/img/`+props.image+`.png`} alt={props.alt} />
         </Tecnologia>
-    );
+    )
 }
 
-export default Item;
+export default Item
